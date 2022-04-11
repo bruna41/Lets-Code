5cb73c196e294f9af57499c25cd7a58a5da6adc8
Bruna <bruna41bergami@gmail.com>
2022-04-11 11:54:31 -0300

    @bruna41 [Fundamentos ReactJS]

---
+++ React/meu-app/src/components/App4.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class App4 extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { nome: undefined, txtNome: '' }
+        // this.changeName = this.changeName.bind(this)
+    }
+    changetxtName = (event) => {
+        this.setState({ txtNome: event.target.value })
+    }
+    persistNome = () => {
+        this.setState({ nome: this.state.txtNome })
+    }
+    render() {
+        if (!this.state.nome) {
+            return (
+                <div>
+                    Nome: <input type='text' value={this.state.nome} onChange={this.changetxtName}></input>
+                    <button onClick={this.persistNome}>Salvar</button>
+                </div>
+            )
+        }else{
+            return (
+                <>
+
+                    <h1>
+                        Olá, {this.state.nome}
+                    </h1>
+                </>
+            )
+        }
+
+    }
+
+    // render() {
+    //     const renderForm = () => {
+    //         return (
+    //             <>
+    //                 Nome: <input type='text' onChange={this.changeTxtNome} />
+    //                 <button onClick={this.persistTxtNome}>Salvar</button>
+    //             </>
+    //         )
+    //     }
+
+    //     const renderText = () => {
+    //         return (
+    //             <>
+    //                 <p>Olá {this.state.nome}</p>
+    //             </>
+    //         )
+    //     }
+
+    //     return !this.state.nome ? renderForm() : renderText()
+    // }
+
+}
+
+export default App4;
